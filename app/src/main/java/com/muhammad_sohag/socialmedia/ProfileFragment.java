0c78fef0539507223ca3598539ab3d212818950b
Emranul-Islam <emranul.islam.sohag@gmail.com>
2020-02-29 01:39:40 +0600

    Profile Design

--- app/src/main/java/com/muhammad_sohag/socialmedia/ProfileFragment.java
+++ app/src/main/java/com/muhammad_sohag/socialmedia/ProfileFragment.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class ProfileFragment extends Fragment {
@@ -14,12 +20,26 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private TextView name,batchName,departmentName;
+    private ImageView profileImage, coverImage;
+    private Button addPost,editProfile;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        name = view.findViewById(R.id.profile_name);
+        batchName = view.findViewById(R.id.profile_batch);
+        departmentName = view.findViewById(R.id.profile_department);
+        profileImage = view.findViewById(R.id.profile_profile_image);
+        coverImage = view.findViewById(R.id.profile_cover_image);
+        addPost = view.findViewById(R.id.profile_add_postBTN);
+        editProfile = view.findViewById(R.id.profile_editBTN);
+
+
+        return view;
     }
 
 
